bbcc5babf8cfd68b6348b68bba3e01b65d875540
jpehkone21 <102893767+jpehkone21@users.noreply.github.com>
2024-02-08 18:48:18 +0200

    HW2 grabbing done

--- Homework1/Assets/RotateCamera.cs
+++ Homework1/Assets/RotateCamera.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class RotateCamera : MonoBehaviour
 
 {
+    
     public GameObject zoomCamera;
     public GameObject mainCamera;
     private float fixedRotation = 0;
+    
 
-    //private Vector3 fixRotation = new Vector3(0, 180, 0);
-
-    // Update is called once per frame
     void Update()
     {
+
         Vector3 mainCamRotation = mainCamera.transform.eulerAngles;
         zoomCamera.transform.eulerAngles = new Vector3(mainCamRotation.x,  mainCamRotation.y, fixedRotation);
-        //zoomCamera.transform.rotation = mainCamera.transform.rotation;
+
     }
 }
